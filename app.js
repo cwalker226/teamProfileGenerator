b857c537732c58db12b4645771277bf3159b9876
Walker, Corey <corey_walker@mcafee.com>
2020-03-28 09:39:51 -0700

    generating the html and writing it to a file

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-​const team = [];
 
+let id = 1;
+const team = [];
 const questions = [
     {
         type: "list",
         name: "empType",
         message: "What type of employee would you like to add?",
-        choice: ['Manager', 'Engineer', 'Intern', 'None']
+        choices: ['Manager', 'Engineer', 'Intern', 'Done']
     }
 ];
 
@@ -78,23 +78,26 @@ function ask() {
     inquirer.prompt(questions).then(answers => {
         if(answers.empType === "Manager"){
             inquirer.prompt(mgrQestions).then(mgrAnswers => {
-                team.push(new Manager(mgrAnswers.name, id, mgrAnswers.email, mgrAnswers.officeNum));
+                team.push(new Manager(mgrAnswers.name, id++, mgrAnswers.email, mgrAnswers.officeNum));
                 ask();
             });
         }else if(answers.empType === "Engineer"){
             inquirer.prompt(engQuestions).then(engAswers => {
-                team.push(new Engineer(engAswers.name, id, engAswers.email, engAswers.github));
+                team.push(new Engineer(engAswers.name, id++, engAswers.email, engAswers.github));
                 ask();
             });
         }else if(answers.empType === "Intern"){
             inquirer.prompt(internQuestions).then(internAnswers => {
-                team.push(new Intern(internAnswers.name, id, internAnswers.email, internAnswers.school));
+                team.push(new Intern(internAnswers.name, id++, internAnswers.email, internAnswers.school));
                 ask();
             });
         }else{
             // const htmlRenderer = new render();
-            // const html = htmlRenderer.render(team);
-            console.log(team);
+            const html = render(team);
+            if(!fs.existsSync(OUTPUT_DIR)){
+                fs.mkdirSync(OUTPUT_DIR);
+            }
+            fs.writeFileSync(outputPath, html);
         }
     });
 }
@@ -102,21 +105,17 @@ function ask() {
 ask();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
